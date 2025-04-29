78c9091f1ca4e9fb98592c28f18f061f313bdb9c
DeyvisonFSilva <Deyvisonper@hotmail.com>
2025-04-28 23:02:40 -0300

    Consertada as estilizações feitas no CSS das páginas/ Carrosel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export function Footer() {
   return (
     <>
-      <footer className="text-center text-white text-sm py-4">
+      <footer className="text-center text-blue-800 text-sm py-4">
         © 2025. Todos os direitos reservados.
       </footer>
     </>
