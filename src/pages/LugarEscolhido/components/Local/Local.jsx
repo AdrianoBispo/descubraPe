2d7d21189c6753752bcbb6861de122dc28125c3f
Adriano Bispo <drianvini10@gmail.com>
2025-05-14 18:42:42 -0300

    Galeria de imagens criada e adicionada no componente 'Local' da rota 'lugar-escolhido.'

--- src/pages/LugarEscolhido/components/Local/Local.jsx
+++ src/pages/LugarEscolhido/components/Local/Local.jsx
@@ -4,26 +4,10 @@ import {
   CardBody,
   Typography,
   Avatar,
+  Rating,
 } from "@material-tailwind/react";
 
 import "./Local.css";
-
-function StarIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="h-5 w-5 text-yellow-700"
-    >
-      <path
-        fillRule="evenodd"
-        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
 
 export function Local() {
   return (
@@ -31,12 +15,14 @@ export function Local() {
       <div>
         <span> Ver todos os lugares </span>
       </div>
-
-      {/* Seção da Galeria
       
-      <div>
-        
-      </div> */}
+      <div className="flex flex-row items-center justify-center gap-1 mr-12 my-6">
+        <img className=" w-2/3" src="https://park.com.br/wp-content/uploads/2018/12/park-hotel-boa-viagem-recife-pe-capela-dourada.png" alt="" />
+        <div className="flex flex-col gap-1 w-1/3">
+          <img src="https://park.com.br/wp-content/uploads/2018/12/park-hotel-boa-viagem-recife-pe-capela-dourada.png" alt="" />
+          <img src="https://park.com.br/wp-content/uploads/2018/12/park-hotel-boa-viagem-recife-pe-capela-dourada.png" alt="" />
+        </div>
+      </div>
 
       <div>
         <h2>Informações</h2>
@@ -67,16 +53,11 @@ export function Local() {
             color="transparent"
             floated={false}
             shadow={false}
-            className="mx-0 flex items-center gap-4 pt-0 pb-2"
+            className="mx-0 flex items-center gap-4 pt-0 pb-2 mr-1"
           >
             <div className="flex w-full flex-col gap-0.5">
               <div className="ml-3 flex items-center gap-0">
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <p className="ml-1">(5)</p>
+                <Rating value={5} readonly />
               </div>
             </div>
             <Typography className="mr-3 text-nowrap" color="blue-gray">
@@ -88,9 +69,9 @@ export function Local() {
               Vale a pena lugar singular
             </Typography>
             <Typography className="text-blue-600 ml-3 mt-2">
-              &quot;I found solution to all my design needs from Creative Tim. I
-              use them as a freelancer in my hobby projects for fun! And its
-              really affordable, very humble guys !!!&quot;
+              Lugar imperdível em Recife. Lindamente adornada em ouro, é
+              possível fazer fotos incríveis. O ingresso dá direito a guia e
+              custa R$ 15,00. Aceitam pix
             </Typography>
 
             <div className="mt-4 flex flex-row items-center">
