cf61e4862f2b01fb1df756de0020e45945dd1623
Adriano Bispo <drianvini10@gmail.com>
2025-05-14 00:24:55 -0300

    Cards reajustados

--- src/components/Cards/BookingCards.jsx
+++ src/components/Cards/BookingCards.jsx
@@ -4,31 +4,32 @@ import {
   CardBody,
   Typography,
   IconButton,
+  Rating,
 } from "@material-tailwind/react";
 
 import { FaHeart } from "react-icons/fa";
 
 import { cidadesBadaladas, dataBookingCards } from "./data";
 
-import "./BookingCards.css"
-
+import "./BookingCards.css";
 
 export function BookingCards() {
   return (
     <div className="container-bookingCards">
-      <h1 className="font-bold titulo">
-        Pernambuco: Cidades Badaladas
-      </h1>
-      <p className="ml-8 text-xl">Explore as cidades mais visitadas do estado</p>
+      <h1 className="font-bold titulo">Pernambuco: Cidades Badaladas</h1>
+      <p className="ml-8 text-xl">
+        Explore as cidades mais visitadas do estado
+      </p>
 
       {cidadesBadaladas.map((cidade) => (
         <div className="inline-block">
-          <Card
-            key={cidade.id}
-            className="w-full max-w-[26rem] shadow-lg m-7"
-          >
+          <Card key={cidade.id} className="w-full max-w-[26rem] shadow-lg m-7">
             <CardHeader floated={false} color="blue-gray">
-              <img src={cidade.image} alt="ui/ux review check" className="w-300 h-30" />
+              <img
+                src={cidade.image}
+                alt="ui/ux review check"
+                className="w-300 h-30"
+              />
 
               <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
               <IconButton
@@ -81,15 +82,20 @@ export function BookingCards() {
                 </Typography>
               </div>
 
-              <Typography color="gray">
-                {cidade.description}
-              </Typography>
+              <Typography color="gray">{cidade.description}</Typography>
             </CardBody>
           </Card>
         </div>
       ))}
 
-      {/* {dataBookingCards.map((dataBookingCard) => (
+      <div className="mt-32 mb-2">
+        <h1 className="font-bold titulo">Pernambuco: Melhores Atrações</h1>
+        <p className="ml-8 text-xl">
+          Conheça os locais mais visitados do estado
+        </p>
+      </div>
+
+      {dataBookingCards.map((dataBookingCard) => (
         <div className="inline-block">
           <Card
             key={dataBookingCard.id}
@@ -127,26 +133,11 @@ export function BookingCards() {
                   >
                     {dataBookingCard.localizacao}
                   </Typography>
+                  <div className="flex gap-1.5 ">
+                    <Rating value={parseInt(dataBookingCard.rating)} readonly />
+                    <p className="text-green-600">({dataBookingCard.rating})</p>
+                  </div>
                 </div>
-
-                <Typography
-                  color="blue-gray"
-                  className="flex items-center gap-1.5 font-normal"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="-mt-0.5 h-5 w-5 text-yellow-700"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  {dataBookingCard.rating}
-                </Typography>
               </div>
 
               <Typography color="gray">
@@ -155,7 +146,7 @@ export function BookingCards() {
             </CardBody>
           </Card>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
--- src/components/Cards/data.js
+++ src/components/Cards/data.js
@@ -37,45 +37,6 @@ export const cidadesBadaladas = [
   },
 ];
 
-export const cidade = [
-  {
-    id: 1,
-    title: "Recife",
-    image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/00/2e/31/recife.jpg?w=600&h=500&s=1",
-    localizacao: "Pernambuco, Brasil",
-    categoria: "cidade",
-    rating: "5.0",
-  },
-  {
-    id: 2,
-    title: "Ipojuca",
-    image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b1/6e/40/20170317-105852-largejpg.jpg?w=600&h=400&s=1",
-    localizacao: "Pernambuco, Brasil",
-    categoria: "cidade",
-    rating: "5.0",
-  },
-  {
-    id: 3,
-    title: "Olinda",
-    image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/81/4e/3c/vista-do-elevador.jpg?w=600&h=400&s=1",
-    localizacao: "Pernambuco, Brasil",
-    categoria: "cidade",
-    rating: "5.0",
-  },
-  {
-    id: 4,
-    title: "Caruaru",
-    image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/ba/36/b5/20160624-155441-largejpg.jpg?w=600&h=400&s=1",
-    localizacao: "Pernambuco, Brasil",
-    categoria: "cidade",
-    rating: "5.0",
-  },
-];
-
 export const dataBookingCards = [
   {
     id: 1,
@@ -167,6 +128,39 @@ export const dataBookingCards = [
   },
   {
     id: 9,
+    title: "Praia dos Carneiros",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
+    localizacao: "Tamandaré, PE",
+    categoria: "Praia",
+    description:
+      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+    rating: "5.0",
+  },
+  {
+    id: 10  ,
+    title: "Praia dos Carneiros",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
+    localizacao: "Tamandaré, PE",
+    categoria: "Praia",
+    description:
+      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+    rating: "5.0",
+  },
+  {
+    id: 11,
+    title: "Praia dos Carneiros",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
+    localizacao: "Tamandaré, PE",
+    categoria: "Praia",
+    description:
+      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+    rating: "5.0",
+  },
+  {
+    id: 12,
     title: "Praia dos Carneiros",
     image:
       "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
