2b0e7d0bf05e252906d0f8dfac586e46186c3568
Adriano Bispo <drianvini10@gmail.com>
2025-05-20 10:56:36 -0300

    Configuração da navlink criada

---
+++ src/mocks/navConfig.js
@@ -0,0 +1,16 @@
+export const navConfig = {
+  "/": [
+    { label: "Início", path: "/" },
+    { label: "Trilhas", path: "/" },
+    { label: "Missões", path: "/missoes" },
+    { label: "Parcerias", path: "/parcerias" },
+    { label: "Perfil", path: "/profile" },
+  ],
+  "/landing-page": [
+    { label: "Início", path: "/" },
+    { label: "Trilhas", path: "/" },
+    { label: "Missões", path: "/missoes" },
+    { label: "Parcerias", path: "/parcerias" },
+    { label: "Assinaturas", path: "/planos" },
+  ]
+};
