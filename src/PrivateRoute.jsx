5ea91c16cdf413cb9c1337a00d29df0b654f6d80
Adriano Bispo <drianvini10@gmail.com>
2025-05-20 10:43:46 -0300

    Contexto de autenticação criado(AuthContext) e lógica de proteção de rotas criada também(PrivateRoute)

---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+// src/PrivateRoute.js
+import { Navigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
+
+const PrivateRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+
+  return currentUser ? children : <Navigate to="/landing-page" />;
+};
+
+export default PrivateRoute;
