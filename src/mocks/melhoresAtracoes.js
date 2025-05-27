69309cd54b784b4875506435864a621a261c844d
DeyvisonFSilva <Deyvisonper@hotmail.com>
2025-05-26 23:58:38 -0300

    Atualizada as informações dos cards de melhores Atrações

--- src/mocks/melhoresAtracoes.js
+++ src/mocks/melhoresAtracoes.js
@@ -1,90 +1,90 @@
 export const melhoresAtracoes = [
   {
     id: "001",
-    title: "Praia dos Carneiros",
+    title: "Restaurante Entre Amigos",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://lh3.googleusercontent.com/p/AF1QipNHPBizGkVnZlpl1SdGc7C4T_nJrHVGVOJM0Lgv=s1360-w1360-h1020-rw",
+    localizacao: "Recife, PE",
+    categoria: "Gastronomia",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "Restaurante tradicional que oferece culinária regional e brasileira, com ambiente familiar e pratos caseiros e saborosos.",
     rating: "5.0",
   },
   {
     id: "002",
-    title: "Praia dos Carneiros",
+    title: "Casa de Noca",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://media-cdn.tripadvisor.com/media/photo-s/08/bf/cf/f9/casa-de-noca.jpg",
+    localizacao: "Olinda, PE",
+    categoria: "Gastronomia",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "Localizado em Olinda, restaurante simples, comida caseira e famosa pela melhor macaxeira gourmet da região. O restaurante está situado numa rua secundária do Sítio Histórico de Olinda, no caminho entre o Alto da Sé e os Quatro Cantos de Olinda.",
     rating: "5.0",
   },
   {
     id: "003",
-    title: "Praia dos Carneiros",
+    title: "Beijupirá",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://lh3.googleusercontent.com/p/AF1QipMPRfit4-_rJjeil02FgsrNsRWUT0S5MRgJdgGV=s1360-w1360-h1020-rw ",
+    localizacao: "Ipojuca, PE",
+    categoria: "Gastronomia",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "Restaurante renomado em Porto de Galinhas, combina frutos do mar com sabores tropicais. O ambiente é rústico, acolhedor e cercado por natureza, oferecendo uma experiência gastronômica sofisticada e regional ao mesmo tempo.",
     rating: "5.0",
   },
   {
     id: "004",
-    title: "Praia dos Carneiros",
+    title: "Bodódromo",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://lh3.googleusercontent.com/p/AF1QipPebOcoR9GbXojS8CktO80oSahLgpD8dmILnF1W=s1360-w1360-h1020-rw",
+    localizacao: "Petrolina, PE",
+    categoria: "Gastronomia",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      " Localizado em Petrolina, o Bodódromo é um complexo gastronômico tradicional, famoso por reunir diversos restaurantes especializados em pratos à base de bode e carneiro. É um dos principais pontos turísticos da cidade e referência da culinária sertaneja no estado de Pernambuco. ",
     rating: "5.0",
   },
   {
     id: "005",
-    title: "Praia dos Carneiros",
+    title: "Capela Dourada",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://www.bahia.ws/wp-content/uploads/2017/10/Capela-Dourada.jpg",
+    localizacao: "Recife, PE",
+    categoria: "Capela",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "A Capela dos Noviços da Ordem Terceira de São Francisco de Assis ou Capela Dourada é uma capela da Ordem Franciscana localizada na cidade do Recife, capital do estado brasileiro de Pernambuco. Com construção iniciada em 1696, foi o primeiro templo do Brasil a ser integralmente coberto de decoração barroca e ouro.",
     rating: "5.0",
   },
   {
     id: "006",
-    title: "Praia dos Carneiros",
+    title: "Capela de São Benedito",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
+      "https://vivoportodegalinhas.com/wp-content/uploads/2022/09/Vivo-Porto-de-Galinhas-Capela-de-Sao-Benedito-ou-Igrejinha-dos-Carneiros-17.png",
     localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+    categoria: "Capela",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "A Capela de São Benedito, construída no século XVIII, é um importante marco histórico e religioso da região. Com sua arquitetura colonial e localização privilegiada, a capela atrai turistas que buscam conhecer um pouco mais sobre a história e a cultura local.",
     rating: "5.0",
   },
   {
     id: "007",
-    title: "Praia dos Carneiros",
+    title: "Paróquia Sant'Ana Gravatá",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/8f/2e/c6/outra-vista-da-atracao.jpg?w=1200&h=-1&s=1",
+    localizacao: "Gravatá, PE",
+    categoria: "Capela",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "é uma igreja católica de grande importância na Diocese de Caruaru. Localizada no centro de Gravatá, é um dos pontos turísticos e religiosos mais importantes da cidade, com uma história de mais de 150 anos. A igreja substituiu uma antiga capela e é conhecida por sua arquitetura com traços barroco, um interior simples e uma torre com traços mais contemporâneos.",
     rating: "5.0",
   },
   {
     id: "008",
-    title: "Praia dos Carneiros",
+    title: "Igreja de São Pedro dos Clérigos",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://www2.recife.pe.gov.br/sites/default/files/styles/imagem_slide_home/public/patio_sao_pedro-fotos_inaldo_lins-17-02-2016_2.jpg?itok=dHQaGF9q  ",
+    localizacao: "Recife, PE",
+    categoria: "Capela",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "A Concatedral de São Pedro dos Clérigos, também conhecida simplesmente como Igreja de São Pedro dos Clérigos, é um templo católico situado no município do Recife, capital do estado de Pernambuco, Brasil. É a consede da Arquidiocese de Olinda e Recife. ",
     rating: "5.0",
   },
   {
@@ -100,35 +100,35 @@ export const melhoresAtracoes = [
   },
   {
     id: "010",
-    title: "Praia dos Carneiros",
+    title: "Passeio de Buggy nas Dunas de Maracaípe",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
+      "https://pgsocial-media.s3.sa-east-1.amazonaws.com/wp-content/uploads/2020/10/27114441/maracaipe-coqueiral-5cae588b-0b2c-4931-be4f-66b9dd6e14fa.jpg https://beachhousemaracaipe.com/wp-content/uploads/2021/08/buggyrides_porto_de_galinhas-1.jpg",
+    localizacao: " Ipojuca, PE",
     categoria: "Praia",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "Passeio emocionante pelas dunas e praias da região.",
     rating: "5.0",
   },
   {
     id: "011",
-    title: "Praia dos Carneiros",
+    title: "Instituto Ricardo Brennand",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
-    categoria: "Praia",
+      "https://rota1976.com/wp-content/uploads/2020/04/instituto-ricardo-brennand.jpg",
+    localizacao: "Várzea, Recife, PE",
+    categoria: "Cultural",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      " O Instituto Ricardo Brennand, também conhecido como Castelo de Brennand, é uma instituição cultural brasileira sem fins lucrativos localizada na cidade do Recife, capital do estado de Pernambuco.",
     rating: "5.0",
   },
   {
     id: "012",
-    title: "Praia dos Carneiros",
+    title: "Cachoeira Véu da Noiva",
     image:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/b9/e6/c6/praia-dos-carneiros-en.jpg?w=2000&h=-1&s=1",
-    localizacao: "Tamandaré, PE",
+      "https://lh3.googleusercontent.com/gps-cs-s/AC9h4noJ_lz59Aayw-om6u_pgx9NxPwRo8LpNi4OwPYL_WqwMsAClvUaY-qLJGpa9aGniQxgysk-Jbf6UsZu2Fr1fK2Md3Plz5nSnPrF6DuR62dAVaQytRZxExxaPi5Y5I2hwExfnVOZ=s1360-w1360-h1020-rw",
+    localizacao: "Bonito, PE",
     categoria: "Praia",
     description:
-      "A Praia dos Carneiros está localizada no município de Tamandaré, em Pernambuco, Brasil. É conhecida por suas águas cristalinas, coqueirais e pela Capela de São Benedito à beira-mar. A praia fica a cerca de 96 km de Recife e a 50 km de Porto de Galinhas.",
+      "Uma das cachoeiras mais famosas de Pernambuco, com 32 metros de queda d’água, ideal para banhos, trilhas e prática de rapel, rodeada por mata atlântica preservada.",
     rating: "5.0",
   },
 ];
